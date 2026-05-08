c7d08f3d194bb982ab205ddcd5ad28692be357ca
wiznethk-git <webmaster@wiznet.hk>
2026-05-08 11:24:03 +0800

    Add files via upload

--- shorts_tutorial/Short Tutorial 18a(UDP_Client).py
+++ shorts_tutorial/Short Tutorial 18a(UDP_Client).py
@@ -1,22 +1,34 @@
-# UDP - Client
-
+# UDP Test - Client
 import network
 from machine import SPI, Pin
 import socket
+import time
 
+NET_IP = "169.254.100.50"
+DEST_IP = "169.254.219.97"
+DEST_PORT = 5000
+
+# Setup Ethernet
 spi = SPI(2, baudrate=8_000_000)
-nic = network.WIZNET5K(spi, Pin("PB12", Pin.OUT), Pin("PD9", Pin.OUT))
-nic.active(True)
-nic.ifconfig(("192.168.1.21", "255.255.255.0", "192.168.1.1", "8.8.8.8"))
+cs = Pin("PB12", Pin.OUT)
+rst = Pin("PD9", Pin.OUT)
 
+nic = network.WIZNET5K(spi, cs, rst)
+nic.active(True)
+nic.ifconfig((NET_IP, "255.255.0.0", "0.0.0.0", "8.8.8.8"))
+
+print(f"W55MH32L-EVB IP: {nic.ifconfig()[0]}")
+
+# Create UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('0.0.0.0', 5000))
 
 while True:
     msg = input("Message: ")
-    if not msg:
+    if msg == "exit":
         break
-    s.sendto(msg.encode(), ("192.168.1.20", 5000))
-    data, addr = s.recvfrom(1024)
-    print(data.decode())
+    s.sendto(msg + "\n", (DEST_IP, DEST_PORT))
+    print(f"Sent: {msg}")
 
 s.close()
+print("Finished!")
--- shorts_tutorial/Short Tutorial 18b(UDP_Server).py
+++ shorts_tutorial/Short Tutorial 18b(UDP_Server).py
@@ -1,19 +1,33 @@
-# UDP - Server
-
+# UDP Test - Server
 import network
 from machine import SPI, Pin
 import socket
+import time
 
+NET_IP = "169.254.100.50"
+DEST_IP = "169.254.219.97"
+DEST_PORT = 5000
+
+# Setup Ethernet
 spi = SPI(2, baudrate=8_000_000)
-nic = network.WIZNET5K(spi, Pin("PB12", Pin.OUT), Pin("PD9", Pin.OUT))
-nic.active(True)
-nic.ifconfig(("192.168.1.20", "255.255.255.0", "192.168.1.1", "8.8.8.8"))
+cs = Pin("PB12", Pin.OUT)
+rst = Pin("PD9", Pin.OUT)
 
+nic = network.WIZNET5K(spi, cs, rst)
+nic.active(True)
+nic.ifconfig((NET_IP, "255.255.0.0", "0.0.0.0", "8.8.8.8"))
+
+print(f"W55MH32L-EVB IP: {nic.ifconfig()[0]}")
+
+# Create UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("192.168.1.20", 5000))
-print("UDP Echo Server Ready")
+s.bind(('0.0.0.0', 5000))
 
 while True:
     data, addr = s.recvfrom(1024)
-    print(f"{addr}: {data.decode()}")
-    s.sendto(b"Echo: " + data, addr)
+    print(f"Response: {data.decode()}")
+    if data.decode() == "exit":
+        break
+
+s.close()
+print("Finished!")
